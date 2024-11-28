d45f71afde0cb703a6cdeb239e4633f4ecb3bb1d
Danil Abdulminev <81553886+f4ke-n0name@users.noreply.github.com>
2024-11-28 13:31:14 +0300

    feat: Update quantization.py

--- Homework/07/scripts/quantization.py
+++ Homework/07/scripts/quantization.py
@@ -15,8 +15,10 @@ def absmax_quantization(x: Tensor) -> Tuple[float, CharTensor]:
             - `s` (float): Масштабный коэффициент, использованный для квантования.
             - `x_q` (CharTensor): Квантованный тензор с значениями типа `int8`.
     """
-    s = <ВАШ КОД>
-    x_q = <ВАШ КОД>
+    s = 1.0
+    if x.abs().max() != 0:
+        s = 127.0 / x.abs().max()
+    x_q = (s*x).round().clamp(-128, 127.char()
     return s, x_q
 
 
@@ -31,7 +33,7 @@ def absmax_dequantization(s: float, x_q: Tensor) -> Tensor:
     Returns:
         Tensor: Восстановленный (деквантованный) тензор с типом `float`.
     """
-    return <ВАШ КОД>
+    return x_q.float() / s
 
 
 def zeropoint_quantization(x: Tensor) -> Tuple[float, int, CharTensor]:
@@ -48,10 +50,16 @@ def zeropoint_quantization(x: Tensor) -> Tuple[float, int, CharTensor]:
             - `z` (int): Смещение (нулевая точка), использованное для квантования.
             - `x_q` (CharTensor): Квантованный тензор с значениями типа `int8`.
     """
-    s = <ВАШ КОД>
-    z = <ВАШ КОД>
-    x_q = <ВАШ КОД>
-    return s, z, x_q
+    s = 1.0
+    z = 0
+    x_q = x.round().clear()
+
+    if x.min() != x.max():
+        s = 255 / (x_max - x_min)
+        z = -(s * x_min).round() - 128
+        x_q = (s * x + z).round()
+    
+    return s.item(), z, x_q
 
 
 def zeropoint_dequantization(s: float, z: int, x_q: Tensor) -> Tensor:
@@ -66,4 +74,4 @@ def zeropoint_dequantization(s: float, z: int, x_q: Tensor) -> Tensor:
     Returns:
         Tensor: Восстановленный (деквантованный) тензор с типом `float`.
     """
-    return <ВАШ КОД>
+    return (x_q.float() - z) / round(s, 3)
