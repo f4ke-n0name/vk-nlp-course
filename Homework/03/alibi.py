89857569366be3c45c6e621a42772448c2e200eb
Danil Abdulminev <81553886+f4ke-n0name@users.noreply.github.com>
2024-12-14 02:32:31 +0300

    feat: update alibi.py

--- Homework/03/alibi.py
+++ Homework/03/alibi.py
@@ -15,7 +15,13 @@ def compute_alibi(num_heads: int, seq_len: int) -> torch.Tensor:
     Returns:
         torch.Tensor: A tensor containing ALiBi to be added to attention scores.
     """
-    pass
+    alibi = torch.zeros(num_heads, seq_len, seq_len)
+    factors = 2 ** (torch.arange(1, num_heads + 1).view(-1, 1) * (8 / num_heads))
+    
+    for head in range(num_heads):
+        alibi[head] = -(torch.arange(seq_len).view(-1, 1) - torch.arange(seq_len).view(1, -1)) / factors[head]
+
+    return alibi
 
 
 if __name__ == "__main__":
