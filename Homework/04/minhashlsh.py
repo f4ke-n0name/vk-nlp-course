d390de82d63db228a41406d74b8c4c91ff476c13
Danil Abdulminev <81553886+f4ke-n0name@users.noreply.github.com>
2024-12-14 20:37:42 +0300

    feat: update minhashlsh.py

--- Homework/04/minhashlsh.py
+++ Homework/04/minhashlsh.py
@@ -14,8 +14,18 @@ class MinHashLSH(MinHash):
         '''
         Возвращает массив из бакетов, где каждый бакет представляет собой N строк матрицы сигнатур.
         '''
-        # TODO:
-        return 
+        step = len(minhash) // self.num_buckets
+        result = []
+        left = 0
+
+        for i in range(self.num_buckets):
+            cur_len = step + (1 if (len(minhash) - left) % (self.num_buckets - i) > 0 else 0)
+            result.append(minhash[left:left + cur_len])
+            left += cur_len
+            if left >= len(minhash):
+                break
+
+        return result
     
     def get_similar_candidates(self, buckets) -> list[tuple]:
         '''
@@ -23,7 +33,20 @@ class MinHashLSH(MinHash):
         Кандидаты похожи, если полностью совпадают мин хеши хотя бы в одном из бакетов.
         Возвращает список из таплов индексов похожих документов.
         '''
-        # TODO:
+        similar_candidates = set()
+        for bucket in buckets:
+            value_to_indices = {}
+            for idx in range(bucket.shape[1]):
+                value_tuple = tuple(bucket[:, idx])
+                if value_tuple not in value_to_indices:
+                    value_to_indices[value_tuple] = []
+                value_to_indices[value_tuple].append(idx)
+
+            for indices in value_to_indices.values():
+                if len(indices) > 1:
+                    for i in range(len(indices)):
+                        for j in range(i + 1, len(indices)):
+                            similar_candidates.add((indices[i], indices[j]))
         return similar_candidates
         
     def run_minhash_lsh(self, corpus_of_texts: list[str]) -> list[tuple]:
@@ -33,4 +56,5 @@ class MinHashLSH(MinHash):
         similar_candidates = self.get_similar_candidates(buckets)
         
         return set(similar_candidates)
+    
     
